5c01713dcf1ee69b7184679f0b03f2e2a0a3acd2
0xSacul <63975424+0xSacul@users.noreply.github.com>
2023-09-03 01:06:52 +0200

    Basic Server handling multiple Islands

---
+++ src/app.config.ts
@@ -0,0 +1,50 @@
+import config from "@colyseus/tools";
+import { monitor } from "@colyseus/monitor";
+import { playground } from "@colyseus/playground";
+import basicAuth from "express-basic-auth";
+
+import { LocalRoom } from "./rooms/localRoom";
+import { IngalsRoom } from "./rooms/ingalsRoom";
+import { ProjectDignityRoom } from "./rooms/projectdignityRoom";
+import { PropHuntRoom } from "./rooms/prophuntRoom";
+
+console.log(process.env.ADMIN_PASS);
+
+const basicAuthMiddleware = basicAuth({
+  // list of users and passwords
+  users: {
+    admin: process.env.ADMIN_PASS,
+  },
+  // sends WWW-Authenticate header, which will prompt the user to fill
+  // credentials in
+  challenge: true,
+});
+
+export default config({
+  initializeGameServer: (gameServer) => {
+    gameServer.define("local", LocalRoom);
+    gameServer.define("project_dignity", ProjectDignityRoom);
+    gameServer.define("prop_hunt", PropHuntRoom);
+    gameServer.define("ingals_room", IngalsRoom);
+  },
+
+  initializeExpress: (app) => {
+    if (process.env.NODE_ENV !== "production") {
+      app.use("/", playground);
+    } else {
+      app.get("/", (req, res) => {
+        res.send("Server is up and running.");
+      });
+    }
+
+    app.use(
+      "/admin",
+      basicAuthMiddleware,
+      monitor({
+        columns: ["roomId", "name", "clients", "elapsedTime"],
+      })
+    );
+  },
+
+  beforeListen: () => {},
+});
