c0d118cf82efe56ca0d7b2bc7c11e28fa05ed8e2
felgaceltica <rodrigo.felga@celtica.com.br>
2024-06-27 15:47:58 -0300

    Voltando publicação

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,25 +2,15 @@ const os = require('os');
 
 module.exports = {
     apps : [{
-      name: "colyseus-app",
-      script: 'build/index.js',
-      time: true,
-      watch: false,
-      instances: os.cpus().length,
-      exec_mode: 'fork',
-      wait_ready: true,
-      env_production: {
-        NODE_ENV: 'production'
-      }
+        name: "colyseus-app",
+        script: 'build/index.js',
+        time: true,
+        watch: false,
+        instances: os.cpus().length,
+        exec_mode: 'fork',
+        wait_ready: true,
+        env_production: {
+            NODE_ENV: 'production'
+        }
     }],
-    deploy : {
-      production : {
-        "user" : "deploy",
-        "host" : ["217.69.5.216"],
-        "ref"  : "origin/main",
-        "repo" : "git@github.com:felgaceltica/portals-server.git",
-        "path" : "/home/deploy",
-        "post-deploy" : "npm install && npm run build && npm exec colyseus-post-deploy"
-      }
-    }
-  };
+};
