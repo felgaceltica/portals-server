234abdefe588cf7ccfef97043f4e14efed63d69d
felgaceltica <rodrigo.felga@celtica.com.br>
2024-06-30 10:27:21 -0300

    Ajuste troca de servidor

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,26 @@
 const os = require('os');
 
-module.exports = {  
-    apps : [{  
-      name: "colyseus-app",  
-      script: 'build/index.js',  
-      time: true,  
-      watch: false,  
-      instances: os.cpus().length,  
-      exec_mode: 'fork',  
-      wait_ready: true,  
-      env_production: {  
-        NODE_ENV: 'production'  
-      }  
-    }],  
-    deploy : {  
-      production : {  
-        "user" : "deploy",  
-        "host" : ["95.179.144.104"],  
-        "ref"  : "origin/main",  
-        "repo" : "git@github.com:felgaceltica/portals-server.git",  
-        "path" : "/home/deploy",  
-        "post-deploy" : "npm install && npm run build && npm run colyseus-post-deploy"  
-      }  
-    }  
-  }; 
+module.exports = {
+    apps : [{
+      name: "colyseus-app",
+      script: 'buil/index.js',
+      time: true,
+      watch: false,
+      instances: os.cpus().length,
+      exec_mode: 'fork',
+      wait_ready: true,
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }],
+    deploy : {
+      production : {
+        "user" : "deploy",
+        "host" : ["95.179.144.104"],
+        "ref"  : "origin/main",
+        "repo" : "git@github.com:felgaceltica/portals-server.git",
+        "path" : "/home/deploy",
+        "post-deploy" : "npm install && npm run build && npm exec colyseus-post-deploy"
+      }
+    }
+  };
