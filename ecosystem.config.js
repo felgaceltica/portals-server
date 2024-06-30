7f6f0777eadba7080e26a8aa32d620787308ff56
felgaceltica <rodrigo.felga@celtica.com.br>
2024-06-30 10:52:24 -0300

    Correção arquivo publicação

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ const os = require('os');
 module.exports = {
     apps : [{
       name: "colyseus-app",
-      script: 'buil/index.js',
+      script: 'build/index.js',
       time: true,
       watch: false,
       instances: os.cpus().length,
